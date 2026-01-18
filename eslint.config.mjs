468e748dc98718b0dc199566b4a2680f4fcf9a5a
Will Hao <williamhao99@gmail.com>
2026-01-18 17:13:29 -0600

    Update ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+  globalIgnores([".next/**", "node_modules/**", "temp-stash/**"]),
+]);
