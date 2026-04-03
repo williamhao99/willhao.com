1ad7437c6faac953bf99bb79be2816eb433885f2
Will Hao <williamhao99@gmail.com>
2026-04-03 14:53:05 -0500

    New link and external link icons

---
+++ components/icons/ExternalLinkIcon.tsx
@@ -0,0 +1,27 @@
+// From: https://lucide.dev/icons/external-link
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  className?: string | undefined;
+}
+
+export default function ExternalLinkIcon({ className, ...props }: IconProps) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M15 3h6v6" />
+      <path d="M10 14 21 3" />
+      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+    </svg>
+  );
+}
